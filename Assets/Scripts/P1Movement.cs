7a6c7c382f465215f5e387abf8d0dd4fa39832da
Truh Anthony <truh.ant8288@mediadesign.school.nz>
2019-05-22 12:54:31 +1200

    P1 Placeholder Movement, Jump and double jump not animated

--- Assets/Scripts/P1Movement.cs
+++ Assets/Scripts/P1Movement.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class P1Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //Variables
+    public float movementSpeed;
+    public float jumpForce;
+    public float moveInput;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+    private Rigidbody2D rb;
+
+
+
     void Start()
     {
-        
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+    //Methods
+    void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround); 
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+    }
+
     void Update()
     {
-        
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && extraJumps > 0) 
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftShift) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
     }
 }
