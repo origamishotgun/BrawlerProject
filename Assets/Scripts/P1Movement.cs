4fbdd9a4197c136c1c0107bca87343e21c1f5c2a
njlawlor <46461983+njlawlor@users.noreply.github.com>
2019-05-24 09:19:52 +1200

    Attack

--- Assets/Scripts/P1Movement.cs
+++ Assets/Scripts/P1Movement.cs
@@ -4,22 +4,69 @@ using UnityEngine;
 
 public class P1Movement : MonoBehaviour
 {
-    public P1MovementController controller;
-    public Animator animator; 
-    // Start is called before the first frame update
+    //Variables
+    public float movementSpeed;
+    public float jumpForce;
+    public float moveInput;
+
+    private bool facingRight = true;
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+    private Rigidbody2D rb;
+  
+
     void Start()
     {
-        
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-     void Update()
+    //Methods
+    void FixedUpdate()
     {
-        
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround); 
+
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * movementSpeed, rb.velocity.y);
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
     }
 
-    public void onLanding()
+    void Update()
     {
-        animator.SetBool("isJumping", false);
+        if(isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+        if (Input.GetKeyDown(KeyCode.LeftShift) && extraJumps > 0) 
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if(Input.GetKeyDown(KeyCode.LeftShift) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
+
     }
 }
